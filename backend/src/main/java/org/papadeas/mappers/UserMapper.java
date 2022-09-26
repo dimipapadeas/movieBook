a40e66647793113485b96f6952741acbb3ea90a1
dimitris.papadeas <Dimitris.Papadeas@eurodyn.com>
2022-09-26 21:38:09 +0300

    [Bugfix-001] Prevent hashed passwords revealing using Api

--- backend/src/main/java/org/papadeas/mappers/UserMapper.java
+++ backend/src/main/java/org/papadeas/mappers/UserMapper.java
@@ -1,11 +1,15 @@
 package org.papadeas.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.papadeas.dto.UserDto;
 import org.papadeas.model.User;
 
 @Mapper(componentModel = "spring")
-public interface UserMapper extends BaseMapper<User, UserDto>{
+public interface UserMapper extends BaseMapper<User, UserDto> {
 
 
+    @Override
+    @Mapping(target = "password", ignore = true)
+    UserDto mapToDTO(User entity);
 }
